ec73c774c31343c7d1300106e6b28c1d9b5c400e
yunminiature <yuliazhmakina@gmail.com>
2022-01-08 20:09:36 +0600

    Состояния (states)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const foodILike=[
-  {
-    'id':1,
-    'name':'борщ',
-    'image':'https://avatars.mds.yandex.net/get-zen_doc/931568/pub_5bd338c19229b700ab50e2c7_5bd33a76580af300ac34702e/scale_1200',
-    'rating': 4.8
-  },
-  {
-    'id':2,
-    'name':'драники',
-    'image':'https://avatars.mds.yandex.net/get-zen_doc/1350031/pub_5b977500602fad00ad9aa181_5b977d12c9e72b00a91db465/scale_1200',
-    'rating': 4.9
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
   }
-]
 
-function Food({name, pic, rating}){
-  return (
-    <div>
-      <h3>Я люблю {name}</h3>
-      <h4>{rating}/5</h4>
-      <img src={pic} alt="picture" />
-    </div>)
-}
+  componentDidMount() {
+    setTimeout(() => {this.setState({isLoading: false})}, 6000)
+  }
 
-Food.propTypes={
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function renderFood(dish){
-  return <Food key={dish.id} name={dish.name} pic={dish.image} rating={dish.rating} />
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(renderFood)}
-    </div>
-  );
+  render() {
+    const {isLoading} = this.state
+    return <div>{isLoading ? "Loading..." : "Страница загружена"}</div>
+  }
 }
 export default App;
