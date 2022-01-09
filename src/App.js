5415f00f25fffada40faf7dccfa433bb50c9ecc2
yunminiature <yuliazhmakina@gmail.com>
2022-01-10 00:56:42 +0600

    Применение стилей

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -20,18 +21,30 @@ class App extends React.Component {
   }
 
   render() {
-    const {isLoading, movies} = this.state
-    return <div>{isLoading ? "Загрузка..." : movies.map(movie => {
-      console.log(movie);
-      return (<Movie 
-        key={movie.id}
-        id={movie.id}
-        title={movie.title}
-        year={movie.year}
-        summary={movie.summary}
-        poster={movie.medium_cover_image}
-      />)
-    })}</div>
-  }
+      const { isLoading, movies } = this.state;
+      return (
+        <section className="container">
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Загрузка...</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+        </section>
+      );
+    }
 }
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({id, title, year, summary, poster}){
-  return <h4>{title}</h4>
+function Movie({id, title, year, summary, poster, genres}){
+  return <div className="movie">
+    <img className="movie-poster" src={poster} alt="{title}" />
+    <div className="movie-column">
+      <h3 className="movie-title">{title}</h3>
+      <h5 className="movie-year">{year}</h5>
+      <ul className="movie-genres">
+        {genres.map((genre, index) => {
+          return (
+            <li key={index} className="genres-genre">
+              {genre}
+            </li>
+          )
+        })}
+      </ul>
+      <p className="movie-summary">{summary}</p>
+    </div>
+  </div>
 }
 
 Movie.propTypes = {
@@ -10,7 +27,8 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie
